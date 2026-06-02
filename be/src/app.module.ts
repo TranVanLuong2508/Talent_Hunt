c3c409f3c7cf320afba1ebcada4df5718d4d7f3b
TranVanLuong <tranvanluong032020@gmail.com>
2026-06-02 11:55:46 +0700

    config get env variable

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SharedModule } from './shared';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SharedModule,
+    AuthenticationModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SharedModule, ApiConfigService } from '@/shared';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Boostrap');
+
+  const configService = app.select(SharedModule).get(ApiConfigService);
+  const APP_PORT = configService.appConfig.port;
+
+  await app.listen(APP_PORT || 3000);
+  logger.warn(`Application is running on: http://localhost:${APP_PORT || 3000}`);
 }
 bootstrap();
